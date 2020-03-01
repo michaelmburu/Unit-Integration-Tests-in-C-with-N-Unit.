cf87752fa56e694555176d98492e73cbadac1ef2
Michael Mburu <michael.ichangai@students.jkuat.ac.ke>
2020-03-01 20:48:27 +0300

    Added Unit & Integration testing project using N-Unit with over 70% test  code coverage.

--- TestNinja/Mocking/BookingHelper.cs
+++ TestNinja/Mocking/BookingHelper.cs
@@ -39,7 +39,7 @@ namespace TestNinja.Mocking
 
     public class Booking
     {
-        public string Status { get; set; }
+        public string Status { get; set; } 
         public int Id { get; set; }
         public DateTime ArrivalDate { get; set; }
         public DateTime DepartureDate { get; set; }
